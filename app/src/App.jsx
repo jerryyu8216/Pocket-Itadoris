348be9684bded58ed0e5662c980a69f9b9aa0002
Jerry Chen Yu <jerrychenyu@MacBook-Pro-3.local>
2024-01-10 18:12:07 -0700

    started vite project

---
+++ app/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+import PocketItadorisApp from "./components/structural/PocketItadorisApp"
+
+function App() {
+
+  return <PocketItadorisApp/>
+}
+
+export default App
---
+++ app/src/components/structural/PocketItadorisApp.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function PocketItadorisApp() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default PocketItadorisApp;
